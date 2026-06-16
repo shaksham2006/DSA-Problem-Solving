f1275513e34087d0bf3bc21f6a915e70a4ed2957
Shaksham Verma <shaksham202006@gmail.com>
2026-06-16 15:42:03 +0530

    Time: 2 ms (50.08%) | Memory: 8.2 MB (54.68%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool isSafe(vector<string>& board, int row, int col, int n) {
+
+        // column
+        for(int i = 0; i < row; i++) {
+            if(board[i][col] == 'Q') return false;
+        }
+
+        // upper-left diagonal
+        for(int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if(board[i][j] == 'Q') return false;
+        }
+
+        // upper-right diagonal
+        for(int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if(board[i][j] == 'Q') return false;
+        }
+
+        return true;
+    }
+
+    void nQueens(vector<string>& board, int row, int n, int& count) {
+        if(row == n) {
+            count++;
+            return;
+        }
+
+        for(int j = 0; j < n; j++) {
+            if(isSafe(board, row, j, n)) {
+                board[row][j] = 'Q';
+
+                nQueens(board, row + 1, n, count);
+
+                board[row][j] = '.';
+            }
+        }
+    }
+
+    int totalNQueens(int n) {
+        vector<string> board(n, string(n, '.'));
+        int count = 0;
+
+        nQueens(board, 0, n, count);
+
+        return count;
+    }
+};
