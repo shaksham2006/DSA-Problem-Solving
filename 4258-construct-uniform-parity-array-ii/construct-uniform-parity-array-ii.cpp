e46f890e63015dc586cdee7d6955dde06344bb78
Shaksham Verma <shaksham202006@gmail.com>
2026-09-03 08:25:31 +0530

    Time: 1 ms (79.58%) | Memory: 165.9 MB (31.94%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -1,28 +1,18 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        bool even=true;
-        bool odd=true;
-        int evencount=0;
-        int oddcount=0;
         int minOdd=INT_MAX;
         int minEven=INT_MAX;
 
         for(int i=0;i<nums1.size();i++){
-            if(nums1[i]%2!=0){
-                even=false;
-                oddcount++;
-                minOdd=min(minOdd,nums1[i]);
-            }else{
-                odd=false;
-                evencount++;
+            if(nums1[i]%2==0)
                 minEven=min(minEven,nums1[i]);
-            }
+            else
+                minOdd=min(minOdd,nums1[i]);
         }
 
-        if(even==true || odd==true) return true;
+        if(minOdd==INT_MAX || minEven==INT_MAX) return true;
 
         return minOdd<minEven;
     }
 };
-//For making any even to odd we need and odd smaller number and even to make any odd even we need to substract a smaller odd number so the smallest one which can we substract to make it must be odd.
