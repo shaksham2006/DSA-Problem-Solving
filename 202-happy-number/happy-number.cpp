fd39fec678f2ff5f64d2cb91235b135893f56b00
Shaksham Verma <shaksham202006@gmail.com>
2026-09-23 18:49:21 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (67.35%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -2,24 +2,20 @@ class Solution {
 public:
     int getSum(int n) {
         int sum = 0;
-
         while (n > 0) {
             int digit = n % 10;
             sum += digit * digit;
             n /= 10;
         }
-
         return sum;
     }
-
     bool isHappy(int n) {
-        unordered_set<int> seen;
-
-        while (n != 1 && seen.find(n) == seen.end()) {
-            seen.insert(n);
-            n = getSum(n);
-        }
-
-        return n == 1;
+        int slow = n;
+        int fast = n;
+        do{
+            slow = getSum(slow);
+            fast = getSum(getSum(fast));
+        }while(slow!=fast);
+        return slow == 1;
     }
 };
